ad39828e1bece7e31e61ac124404c4d54c2f9f90
TolaMironcenko <mironcenkotola@gmail.com>
2022-06-18 05:34:41 +0700

    add iadd mull

--- fraction.py
+++ fraction.py
@@ -4,17 +4,30 @@ class Fraction:
         self.A = a
         self.B = b
 
-    def display(self):
+    def __str__(self):
         if self.A >= self.B:
             if self. A % self.B == 0:
-                print(self. A // self.B)
+                return '{}'.format(self.A//self.B)
             else:
-                print(self.A // self.B, " and ", self. A % self.B, '/', self.B)
+                return '{} and {}/{}'.format(self.A // self.B, self. A % self.B, self.B)
         else:
-            print(self.A, '/', self.B)
+            return '{}/{}'.format(self.A, self.B)
+        return
+
+    def __add__(self, other):
+        if self.B == other.B:
+            return Fraction(self.A+other.A, self.B)
+        else:
+            return Fraction(self.A*other.B+other.A*self.B, self.B*other.B)
+
+    def __iadd__(self, other):
+        if self.B == other.B:
+            self.A += other.A
+        else:
+            self.A = self.A*other.B+other.A*self.B
+            self.B *= other.B
+        return self
 
     def __mul__(self, fraction):
-        self.A *= fraction.A
-        self.B *= fraction.B
-
+        return Fraction(self.A*fraction.A, self.B*fraction.B)
 
--- main.py
+++ main.py
@@ -2,11 +2,14 @@ from fraction import Fraction
 
 
 def print_hi():
-    number1 = Fraction(3, 2)
-    number1.display()
-    number2 = Fraction(1, 2)
-    number2 = number2*number1
+    number1 = Fraction(2, 3)
+    print(number1)
+    number2 = number1
     print(number2)
+    number2 *= number1
+    print(number2)
+    number3 = number1 + number2
+    print(number3)
 
 
 if __name__ == '__main__':
