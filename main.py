5ba664b60761abc163a6085e8e74d16005c04931
tola <79221297+TolaMironcenko@users.noreply.github.com>
2022-06-25 15:59:06 +0700

    t

--- main.py
+++ main.py
@@ -3,12 +3,11 @@ from polinom import Polinom
 
 
 def main():
-    pol1 = Polinom(3, [4, 3, 2, 9])
-    pol2 = Polinom(4, [1, 2, 3, 4, 5])
+    pol1 = Polinom(2, [1,5,2])
+    pol2 = Polinom(3, [6,1,4,3])
     print(pol1, pol2, sep='\n')
+    print(pol1+pol2)
     print(pol1-pol2)
-    pol1 -= pol2
-    print(pol1)
 
 
 if __name__ == '__main__':
--- polinom.py
+++ polinom.py
@@ -8,6 +8,18 @@ class Polinom:
             if len(array) == n+1:
                 self.array = array
                 self.n = n
+                self.degreestr = {
+                    "0": "⁰",
+                    "1": "¹",
+                    "2": "²",
+                    "3": "³",
+                    "4": "⁴",
+                    "5": "⁵",
+                    "6": "⁶",
+                    "7": "⁷",
+                    "8": "⁸",
+                    "9": "⁹"
+                }
             else:
                 cprint('Error: Длинна массива array должна быть равной n', 'red')
         except Exception as e:
@@ -20,15 +32,15 @@ class Polinom:
                 if self.array[0] == 0:
                     pass
                 else:
-                    result += '{}'.format(self.array[i])
+                    result += ' {} '.format(self.array[i])
                     if i != self.n and self.array[i+1] != 0 and self.array[i+1] > 0:
                         result += '+'
             elif i == 1 and self.array[i] != 0:
-                result += '{}*x'.format(self.array[i])
+                result += ' {}·x '.format(self.array[i])
                 if i != self.n and self.array[i+1] != 0 and self.array[i+1] > 0:
                     result += '+'
             elif self.array[i] != 0:
-                result += '{}*x^{}'.format(self.array[i], i)
+                result += ' {}·x{} '.format(self.array[i], self.degreestr[str(i)])
                 if i != self.n and self.array[i+1] != 0 and self.array[i+1] > 0:
                     result += '+'
         return result
@@ -68,28 +80,40 @@ class Polinom:
         return self
 
     def __sub__(self, other):
-        if other.n >= self.n:
+        if other.n > self.n:
             result_n = other.n
             result_array = [0]*(result_n+1)
             for i in range(self.n+1):
                 result_array[i] = self.array[i] - other.array[i]
             result_array[result_n] = -other.array[result_n]
+            return Polinom(result_n, result_array)
+        elif self.n == other.n:
+            result_array = [0]*(self.n+1)
+            for i in range(self.n+1):
+                result_array[i] = self.array[i] - other.array[i]
+            return Polinom(self.n, result_array)
         else:
             result_n = self.n
             result_array = [0] * (result_n + 1)
             for i in range(other.n + 1):
                 result_array[i] = self.array[i] - other.array[i]
             result_array[result_n] = self.array[result_n]
-        return Polinom(result_n, result_array)
+            return Polinom(result_n, result_array)
+
 
     def __isub__(self, other):
-        if other.n >= self.n:
+        if other.n > self.n:
             result_n = other.n
             result_array = [0]*(result_n+1)
             for i in range(self.n+1):
                 result_array[i] = self.array[i] - other.array[i]
             result_array[result_n] = -other.array[result_n]
             self.n = result_n
+            self.array = result_array
+        elif self.n == other.n:
+            result_array = [0]*(self.n+1)
+            for i in range(self.n+1):
+                result_array[i] = self.array[i] - other.array[i]
             self.array = result_array
         else:
             result_n = self.n
